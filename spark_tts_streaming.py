bd9fd5baae35114e8da87c960abdc8616e6bd8cf
Simon Allan Achuka <87936175+achuka99@users.noreply.github.com>
2026-03-09 04:40:20 +0300

    changed to port 8001

--- spark_tts_streaming.py
+++ spark_tts_streaming.py
@@ -1469,11 +1469,12 @@ if __name__ == "__main__":
     uvicorn.run(
         "spark_tts_streaming:app",
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         reload=False,
         ws_ping_interval=20,
         ws_ping_timeout=20,
         timeout_keep_alive=300
 
     )
+
 
