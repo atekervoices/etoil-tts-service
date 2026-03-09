fc3d45210f7d5c890afec6f410f1f7d864938ee8
Simon Allan Achuka <87936175+achuka99@users.noreply.github.com>
2026-03-09 06:02:36 +0300

    Update speaker ids mapping

--- spark_tts_streaming.py
+++ spark_tts_streaming.py
@@ -48,12 +48,130 @@ DEFAULT_SPEAKER_ID = 243  # Runyankore female
 
 # Speaker IDs mapping
 SPEAKER_IDS = {
-    "acholi_female": 241,
-    "ateso_female": 242,
-    "runyankore_female": 243,
-    "lugbara_female": 245,
-    "swahili_male": 246,
-    "luganda_female": 248,
+    # Acholi
+    "ach_female_242": "ach_female_242",
+    "ach_female_2": "ach_female_2",
+    "ach_female_6": "ach_female_6",
+    "ach_female_7": "ach_female_7",
+    "ach_female_8": "ach_female_8",
+    "ach_male_1": "ach_male_1",
+    "ach_male_3": "ach_male_3",
+    "ach_male_4": "ach_male_4",
+    "ach_male_5": "ach_male_5",
+
+    # Ateso
+    "teo_female_241": "teo_female_241",
+
+    # Runyankore (nyn)
+    "nyn_female_248": "nyn_female_248",
+    "nyn_female_1": "nyn_female_1",
+    "nyn_female_2": "nyn_female_2",
+    "nyn_female_4": "nyn_female_4",
+    "nyn_female_5": "nyn_female_5",
+    "nyn_male_3": "nyn_male_3",
+    "nyn_male_6": "nyn_male_6",
+    "nyn_male_7": "nyn_male_7",
+    "nyn_male_8": "nyn_male_8",
+
+    # Lugbara
+    "lgg_female_245": "lgg_female_245",
+
+    # Swahili
+    "swa_male_246": "swa_male_246",
+    "swa_male_3": "swa_male_3",
+    "swa_male_4": "swa_male_4",
+    "swa_male_8": "swa_male_8",
+    "swa_female_1": "swa_female_1",
+    "swa_female_2": "swa_female_2",
+    "swa_female_5": "swa_female_5",
+    "swa_female_6": "swa_female_6",
+
+    # Luganda
+    "lug_female_4": "lug_female_4",
+    "lug_female_5": "lug_female_5",
+    "lug_female_7": "lug_female_7",
+    "lug_female_8": "lug_female_8",
+    "lug_male_1": "lug_male_1",
+    "lug_male_2": "lug_male_2",
+    "lug_male_3": "lug_male_3",
+    "lug_male_6": "lug_male_6",
+
+    # Kinyarwanda
+    "kin_female_1": "kin_female_1",
+
+    # Luo
+    "luo_female_1": "luo_female_1",
+    "luo_female_2": "luo_female_2",
+    "luo_female_3": "luo_female_3",
+    "luo_female_4": "luo_female_4",
+    "luo_male_1": "luo_male_1",
+    "luo_male_2": "luo_male_2",
+    "luo_male_3": "luo_male_3",
+    "luo_male_4": "luo_male_4",
+
+    # Kikuyu
+    "kik_female_2": "kik_female_2",
+    "kik_female_3": "kik_female_3",
+    "kik_male_1": "kik_male_1",
+    "kik_male_4": "kik_male_4",
+
+    # Hausa
+    "hau_female_1": "hau_female_1",
+    "hau_female_3": "hau_female_3",
+    "hau_female_5": "hau_female_5",
+    "hau_female_7": "hau_female_7",
+    "hau_male_2": "hau_male_2",
+    "hau_male_4": "hau_male_4",
+    "hau_male_6": "hau_male_6",
+    "hau_male_8": "hau_male_8",
+
+    # Igbo
+    "ibo_female_1": "ibo_female_1",
+    "ibo_female_3": "ibo_female_3",
+    "ibo_female_5": "ibo_female_5",
+    "ibo_female_7": "ibo_female_7",
+    "ibo_male_2": "ibo_male_2",
+    "ibo_male_4": "ibo_male_4",
+    "ibo_male_6": "ibo_male_6",
+    "ibo_male_8": "ibo_male_8",
+
+    # Twi
+    "twi_female_2": "twi_female_2",
+    "twi_female_4": "twi_female_4",
+    "twi_male_1": "twi_male_1",
+    "twi_male_3": "twi_male_3",
+
+    # Yoruba
+    "yor_female_1": "yor_female_1",
+    "yor_female_3": "yor_female_3",
+    "yor_female_5": "yor_female_5",
+    "yor_female_7": "yor_female_7",
+    "yor_male_2": "yor_male_2",
+    "yor_male_4": "yor_male_4",
+    "yor_male_6": "yor_male_6",
+    "yor_male_8": "yor_male_8",
+
+    # Wolof
+    "wol_male_1": "wol_male_1",
+    "wol_female_1": "wol_female_1",
+
+    # Nigerian Pidgin
+    "pcm_female_1": "pcm_female_1",
+    "pcm_female_2": "pcm_female_2",
+    "pcm_female_3": "pcm_female_3",
+    "pcm_female_4": "pcm_female_4",
+    "pcm_female_5": "pcm_female_5",
+    "pcm_male_1": "pcm_male_1",
+    "pcm_male_2": "pcm_male_2",
+    "pcm_male_3": "pcm_male_3",
+    "pcm_male_4": "pcm_male_4",
+    "pcm_male_5": "pcm_male_5",
+
+    # Fula
+    "fat_female_2": "fat_female_2",
+    "fat_female_4": "fat_female_4",
+    "fat_male_1": "fat_male_1",
+    "fat_male_3": "fat_male_3",
 }
 
 # Precomputed global tokens for each speaker
@@ -1476,5 +1594,6 @@ if __name__ == "__main__":
         timeout_keep_alive=300
 
     )
+
 
 
